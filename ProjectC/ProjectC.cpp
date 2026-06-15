dc974efaea52b6c82dc61252be0ee958cb08c33d
DuckJeans <lemonfox223@gmail.com>
2026-06-15 19:44:31 +0900

    (2026.6.15) 원형 연결 리스트

--- ProjectC/ProjectC.cpp
+++ ProjectC/ProjectC.cpp
@@ -26,26 +26,101 @@ public:
         tail = nullptr;
     }
 
+    ~List();
+
     void push_front(T data)
     {
         Node* newNode = new Node;
 
         newNode->data = data;
         newNode->previous = nullptr;
-        newNode->head = head;
+        newNode->next = head;
 
         if (head == nullptr)
         {
-            head == nullptr;
-            tail == nullptr;
+            head = nullptr;
+            tail = nullptr;
         }
         else
         {
             head->previous = newNode;
+            newNode->next = head;
             head = newNode;
         }
         
         size++;
+    }
+
+    void pop_front()
+    {
+        if (head == nullptr)
+        {
+            printf("Linked list is empty");
+            return;
+        }
+
+        Node* deleteNode = head;
+
+        if (head == tail)
+        {
+            head = nullptr; 
+            tail = nullptr;
+        }
+        else
+        {
+            deleteNode->next->previous = nullptr;
+            head = head->next;
+        }
+
+        delete deleteNode;
+        size--;
+    }
+
+    void push_back(T data)
+    {
+        Node* newNode = new Node;
+
+        newNode->data = data;
+        newNode->next = nullptr;
+        newNode->previous = nullptr;
+
+        if (head == nullptr)
+        {
+            head = nullptr;
+            tail = nullptr;
+        }
+        else
+        {
+            tail->next = newNode;
+            newNode->previous = tail;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    void pop_back()
+    {
+        if (tail == nullptr)
+        {
+            printf("Linked list is empty");
+            return;
+        }
+
+        Node* deleteNode = tail;
+
+        if (head == tail)
+        {
+            head = nullptr;
+            tail = nullptr;
+        }
+        else
+        {
+            deleteNode->previous->next = nullptr;
+            tail = tail->next;
+        }
+
+        delete deleteNode;
+        size--;
     }
 };
 
@@ -55,6 +130,13 @@ int main()
 
     list.push_front(5);
     list.push_front(1);
+
+    list.push_back(20);
+    list.push_back(30);
+
+    list.pop_front();
+    list.pop_front();
+    list.pop_front();
 
     return 0;
 }
