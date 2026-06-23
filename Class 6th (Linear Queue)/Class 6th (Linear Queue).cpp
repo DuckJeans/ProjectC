54e7d22e1c32ec9436341377c7ae4798a815284d
DuckJeans <lemonfox223@gmail.com>
2026-06-23 19:44:54 +0900

    (2026.6.23) 우선순위 큐

---
+++ Class 6th (Linear Queue)/Class 6th (Linear Queue).cpp
@@ -0,0 +1,97 @@
+﻿#include <iostream>
+
+#define SIZE 5
+
+using namespace std;
+
+template <typename T>
+
+class Queue
+{
+private:
+    int m_front;
+    int m_rear;
+
+    T container[SIZE];
+public:
+    Queue()
+    {
+        m_front = 0;
+        m_rear = 0;
+
+        for (int i = 0; i < SIZE; i++)
+        {
+            container[i] = NULL;
+        }
+    }
+
+    void push(T data)
+    {
+        if (m_rear >= SIZE)
+        {
+            cout << "linear queue overflow" << endl;
+            return;
+        }
+
+        container[m_rear++] = data;
+    }
+
+    void pop()
+    {
+        if (m_front >= m_rear)
+        {
+            cout << "linear queue is empty" << endl;
+            return;
+        }
+
+        container[m_front++] = 0;
+
+        // 프론트가 증가
+        // 10의 데이터를 지운다. -> 0으로
+        // 프론트의 값을 증가시켰기에 한칸 이동
+        // 만약 리어를 30까지만 만들었다고 했을때,
+        // 20삭제(0으로 한후 프론트 증가), 30에 프론트
+        // pop을 했을 때 30에 있던 프론트가 30과 40사이에 있음.
+        // 40에는 리어가 있지만, 프론트가 pop을 한 번 더 실행할 시 
+        // "linear queue is empty"라고 출력.
+        // 리어가 있는 값(40)전 30에 있던 프론트가 pop을 한 번 더 실행하면
+        // 출력하게 됨.
+    }
+
+    const bool& empty()
+    {
+        return m_front >= m_rear;
+    }
+
+    const T& front()
+    {
+        return container[m_front];
+    }
+
+    const int& size()
+    {
+        return m_rear - m_front;
+    }
+};
+
+int main()
+{
+    Queue<int> queue;
+
+    queue.push(10);
+    queue.push(20);
+    queue.push(30);
+    queue.push(40);
+    queue.push(50);
+
+    while (queue.empty() == false)
+    {
+        cout << queue.front() << endl;
+
+        queue.pop();
+    }
+
+    queue.push(1);
+
+    return 0;
+}
--- ProjectC/ProjectC.cpp
+++ ProjectC/ProjectC.cpp
@@ -1,102 +1,34 @@
 ﻿#include <iostream>
 
+#define SIZE 4
+
 using namespace std;
 
 template <typename T>
 
-class Stack
+class Queue
 {
 private:
-    T *container;
-    int capacity;
-    int highset;
+    int m_rear;
+    int m_front;
 
+    T container[SIZE];
 public:
-    Stack()
+    Queue()
     {
-        capacity = 0;
-        highset = -1;
-        container = nullptr;
-    }
+        m_rear = SIZE - 1;
+        m_front = SIZE - 1;
 
-    void resize(int newSize)
-    {
-        T* temporary = new T[capacity];
-
-        for (int i = 0; i < capacity; i++)
+        for (int i = 0; i < SIZE; i++)
         {
-            temporary[i] = NULL;
+            container[i] = NULL;
         }
-
-        for (int i = 0; i < highset; i++)
-        {
-            temporary[i] = container[i];
-        }
-
-        delete[] container;
-
-        container = temporary;
-    }
-
-    void push(T data)
-    {
-        if (capacity <= 0)
-        {
-            resize(1);
-        }
-        else if (highset + 1 >= capacity)
-        {
-            resize(capacity * 2);
-        }
-
-        container[++highset] = data;
-    }
-
-    void pop()
-    {
-        if (highset < 0)
-        {
-            cout << "Stack is empty" << endl;
-        }
-        else
-        {
-            container[highset--] = NULL;
-        }
-    }
-
-    void bool& empty()
-    {
-        return highset == -1;
-    }
-
-    const int& size()
-    {
-        return highset + 1;
-    }
-
-    const T& top()
-    {
-        return container[highset];
-    }
-
-    ~Stack()
-    {
-        delete container[];
     }
 };
 
 int main()
 {
-    Stack<int> stack;
-
-    stack.push(10);
-    stack.push(20);
-    stack.push(30);
-
-    stack.pop();
-    stack.pop();
-    stack.pop();
-    stack.pop();
+    Queue<int> queue;
 
     return 0;
 }
