1f9ab956bf706b2ad80b2e923ce8038f3a695bb5
DuckJeans <lemonfox223@gmail.com>
2026-06-25 19:44:01 +0900

    (2026.6.25) 해시 테이블

--- ProjectC/ProjectC.cpp
+++ ProjectC/ProjectC.cpp
@@ -43,6 +43,85 @@ public:
 
         container = temporary;
     }
+
+    void push(T data)
+    {
+        if (capacity == 0)
+        {
+            resize(1);
+        }
+        else if (index >= capacity)
+        {
+            resize(capacity * 2);
+        }
+
+        container[index] = data;
+
+        int child = index - 1;
+        int parent = (child - 1) / 2;
+        index++;
+
+        while (child > 0 && container[child] > container[parent])
+        {
+            if (container[parent] < containerpchild)
+            {
+                swap(container[parent], container[child]);
+            }
+            child = parent;
+        }
+    }
+
+    void pop()
+    {
+        if (index== 0)
+        {
+            cout << "priority queue is empty" << end;
+        }
+
+        container[0] = container[--index];
+        container[index] = NULL;
+
+        int parent = 0;
+        int child = parent * 2 + 1;
+
+        while (true)
+        {
+            child = parent * 2 + 1;
+
+            if (container[child] < container[child + 1])
+            {
+                child++;
+            }
+
+            if (container[child] < container[parent])
+            {
+                break;
+            }
+            else
+            {
+                swap(container[child], container[parent]);
+
+                parent = child;
+            }
+        }
+    }
+
+    const bool& empty()
+    {
+        reutrn 
+    }
+    const int& size()
+    {
+
+    }
+    const T& top()
+    {
+
+    }
+    ~PriorityQueue( )
+    {
+
+    }
 };
 
 int main()
