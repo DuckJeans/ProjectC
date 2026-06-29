531f2cd09898916710759848309cea76bc509d63
DuckJeans <lemonfox223@gmail.com>
2026-06-29 19:43:27 +0900

    (2026.6.29) 트리

--- ProjectC/ProjectC.cpp
+++ ProjectC/ProjectC.cpp
@@ -6,7 +6,7 @@ template <typename KEY, typename VALUE>
 
 class HashTable
 {
-public:
+private:
     struct Node
     {
         Node* next;
@@ -20,10 +20,17 @@ public:
         int count;
     };
 
+    int size;
+    int capacity;
+    Bucket* bucket;
+
+public:
+    
+
     HashTable()
     {
         size = 0;
-        capacity = 7;
+        capacity = 0;
         bucket = new Bucket[capacity];
 
         for (int i = 0; i < capacity; i++)
@@ -39,24 +46,75 @@ public:
         return (unsigned int)key % capacity;
     }
 
-    template<typename KEY>
+    template<>
     unsigned int hash_function(const char* key)
     {
-        return hash
+        unsigned int sum = 0;
+
+        for (int i = 0; *key != '\0'; i++)
+        {
+            sum += key[i];
+
+            key = key + 1;
+        }
+
+        return sum % capacity;
     }
 
-private:
-    int size;
-    int capacity;
-    Bucket* bucket;
+    void insert(KEY key, VALUE value)
+    {
+        int hashIndex;
+
+        hashIndex = hash_function(key);
+
+        Node* newNode = new Node;
+        newNode->key = key;
+        newNode->value = value;
+        newNode->next = nullptr;
+
+        if (bucket[hashIndex].head == nullptr)
+        {
+            bucket[hashIndex].head = newNode;
+        }
+        else
+        {
+            newNode->next = bucket[hashIndex].head;
+
+            bucket[hashIndex].head = newNode;
+        }
+
+        bucket[hashIndex].count++;
+        size++;
+    }
+
+    ~HashTable()
+    {
+        for (int i = 0; i < capacity; i++)
+        {
+            Node* current = bucket[i].head;
+
+            while (current != nullptr)
+            {
+                Node* deleteNode = current;
+                current = current->next;
+                delete deleteNode;
+            }
+        }
+
+        if (bucket != nullptr)
+        {
+            delete[] bucket;
+        }
+    }
 };
 
 int main()
 {
-    HashTable<float,int> hT;
+    HashTable<const char*,int> hT;
 
-    cout << hT.hash_function(3129) << endl;
-    cout << hT.hash_function(-362) << endl;
+    hT.insert("Knight", 3000);
+    hT.insert("Mega Knight", 8000);
+    hT.insert("Dark Knight", 25000);
 
     return 0;
 }
